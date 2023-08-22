b3d2d810c7e5584e14a46a73c69b0c82d9de60b6
shanmage <thishan1704@gmail.com>
2023-08-22 09:34:48 +0530

    Custom theme with bootstrap

--- app/code/Gmc/Catalog/Block/CategoryProducts.php
+++ app/code/Gmc/Catalog/Block/CategoryProducts.php
@@ -3,8 +3,8 @@
 namespace Gmc\Catalog\Block;
 
 use Magento\Framework\View\Element\Template;
-use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\Pricing\PriceCurrencyInterface;
+use Magento\Framework\UrlInterface;
 
 class CategoryProducts extends Template
 {
@@ -13,47 +13,78 @@ class CategoryProducts extends Template
     protected $productRepository;
 
     protected $priceCurrency;
+    protected $urlInterface;
 
     public function __construct(
         Template\Context $context,
         \Magento\Catalog\Model\CategoryFactory $categoryFactory,
-        ProductRepositoryInterface $productRepository,
         PriceCurrencyInterface $priceCurrency,
+        UrlInterface $urlInterface,
         array $data = []
     ) {
         parent::__construct($context, $data);
         $this->_categoryFactory = $categoryFactory;
-        $this->productRepository = $productRepository;
         $this->priceCurrency = $priceCurrency;
+        $this->urlInterface = $urlInterface;
     }
 
     /**
      * @param $categoryId
-     * @return \Magento\Framework\Data\Collection\AbstractDb
+     * @return string
      */
     public function getCategoryProducts($categoryId)
     {
         $category = $this->_categoryFactory->create()->load($categoryId);
-        return $category->getProductCollection();
-    }
+        $productCollection = $category->getProductCollection()
+            ->addAttributeToSelect('*')
+            ->addAttributeToFilter('status', \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
 
-    public function getProductById($productId)
-    {
-        try {
-            return $this->productRepository->getById($productId);
-        } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
-            return null;
+        // Create and return HTML for displaying products in the tab
+        $html = '<div class="row">';
+        foreach ($productCollection as $product) {
+            $html .= '<div class="col-md-4 mb-4">';
+            $html .= '<div class="card">';
+            $html .= '<img src="' . $this->getImageUrl($product, 'thumbnail') . '" class="card-img-top" alt="' . $product->getName() . '">';
+            $html .= '<div class="card-body">';
+            $html .= '<h5 class="card-title">' . $product->getName() . '</h5>';
+            $html .= '<p class="card-text">' . $this->getFormattedPriceWithCurrency($product->getFinalPrice()) . '</p>';
+            $html .= '<a href="' . $this->getProductUrl($product) . '" class="btn btn-primary">Book Now</a>';
+            // Add more product information as needed
+            $html .= '</div></div></div>';
         }
+        $html .= '</div>';
+
+        return $html;
     }
 
+    /**
+     * @param $product
+     * @param $imageType
+     * @return string
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
     public function getImageUrl($product, $imageType)
     {
         return $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA)
             . 'catalog/product' . $product->getImage();
     }
 
+    /**
+     * @param $price
+     * @return string
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
     public function getFormattedPriceWithCurrency($price)
     {
         return $this->priceCurrency->format($price, false, PriceCurrencyInterface::DEFAULT_PRECISION, $this->_storeManager->getStore());
+    }
+
+    /**
+     * @param $product
+     * @return string
+     */
+    public function getProductUrl($product)
+    {
+        return $this->urlInterface->getUrl('catalog/product/view', ['id' => $product->getId()]);
     }
 }
